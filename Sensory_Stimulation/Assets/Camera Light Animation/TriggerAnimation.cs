52e2fd019004062ec8690dbe54c1f292b6840148
Luke Kim <kiml803@newschool.edu>
2023-04-07 13:59:14 -0400

    Fixed Lighting, Camera Contour Flash Design, Added Animation and script for Light, Added Map Layout,

---
+++ Sensory_Stimulation/Assets/Camera Light Animation/TriggerAnimation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+
+
+{
+    Animator anim1;
+ 
+    void Start()
+    {
+        anim1 = GetComponent<Animator>();
+    }
+ 
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            anim1.SetTrigger("Trigger");
+        }
+    }
+        
+    
+}
